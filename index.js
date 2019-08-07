b41c0dfd8fcb746c8bba291a462a8e5491d32ba9
Himanshu <singh.himanshu3535@gmail.com>
2019-08-07 17:18:28 +0530

    Adding model and schema

--- index.js
+++ index.js
@@ -12,20 +12,33 @@ mongoose.connect('mongodb://localhost/playground')
 // Creating schema using mongoose. Schema is mongoose's concept NOT mongodb's. 
 const courseSchema = new mongoose.Schema({
     name: String,
-    author: String, 
+    author: String,
     tags: [String],
     date: { type: Date, default: Date.now },
     isPublished: Boolean
 });
 
 // Creating new model
+// Model's name should be in singular (Course). Mongo will automatically make it plural (Courses).  
 const Course = mongoose.model('Course', courseSchema);
 
-// making instance of created model to be saved into database. 
-const course = new Course({
-    name: "NodeJS Course",
-    author: "Himanshu",
-    tags: ['nodejs', 'expressjs'],
-    isPublished: true
-});
+async function createCourse() {
+    // making instance of created model to be saved into database. 
+    const course = new Course({
+        name: "NodeJS Course",
+        author: "Himanshu",
+        tags: ['nodejs', 'expressjs'],
+        isPublished: true
+    });
+
+    // Saving course instance into db. 
+    // save() function is an async function so using await and async. 
+    const result = await course.save(); 
+    console.log(result);
+}
+
+// calling function to create a new course
+createCourse(); 
+
+
 
